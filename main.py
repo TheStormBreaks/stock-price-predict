d3008c4bb286cfe7715ef11f6bfaab36cdc08e04
Somya <m2somya@gmail.com>
2024-12-03 22:39:03 +0530

    stock_data.csv

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
+# Fetching the stock data from yfinance api
+ticker = 'GOOG'  # Company name
+start_date = '2020-01-01'
+end_date = '2024-01-01'
 
+stock_data = yf.download(ticker, start = start_date, end = end_date)
+stock_data.to_csv('stock_data.csv')
 
