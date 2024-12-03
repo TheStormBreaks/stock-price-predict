313f57d78d1a681891e6228d14e785bcdd335208
Somya <m2somya@gmail.com>
2024-12-03 22:36:18 +0530

    imports

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+
 
